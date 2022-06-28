73eeef4746ca892e5b5cffac5b0f79559eff943c
rw <6587rajvirani@gmail.com>
2022-06-28 15:49:29 +0530

    28/06/2022

--- lib/screen/home/view/contactspage.dart
+++ lib/screen/home/view/contactspage.dart
@@ -15,7 +15,7 @@ class _con_ScreenState extends State<con_Screen> {
 
   List contacts_Name=["Adal Schmidt","Alan Smith","Amelia Jones","Beyan Davis","Blake Weaver","Christina Johnson","David Wilson","Elizabeth Taylor","Hardik Pandya","Jasprit Bumrah","KL Rahul","MS Dhoni","Odin Son Thor","Ravindra Jadeja","Rishabh Pant","Rohit Sharma","The Rock","Tony Stark","Virat Kohli","Yuzvendra Chahal"];
   List contacts_Image=["assets/image/m1.jpeg","assets/image/m2.jpeg","assets/image/m3.webp","assets/image/m4.jpeg","assets/image/m5.jpeg","assets/image/m6.webp","assets/image/m8.jpeg","assets/image/m9.jpeg","assets/image/m11.jpeg","assets/image/m13.webp","assets/image/m14.jpeg","assets/image/m15.jpeg","assets/image/m12.webp","assets/image/m16.jpeg","assets/image/m18.jpeg","assets/image/m17.jpeg","assets/image/m7.jpeg","assets/image/m10.jpeg","assets/image/m19.jpeg","assets/image/m20.jpeg"];
-  List contacts_Namber=["919514787481","914597682105","917899455600","918521479606","917894895216","912000089547","914577890350","911029378224","914367673458","911258746932","918521479630","918529631470","915252354789","910021458745","918521479630","919632147852","917894215464","916663425175","912000147835","911258749630"];
+  List contacts_Namber=["9514787481","4597682105","7899455600","8521479606","7894895216","2000089547","4577890350","1029378224","4367673458","1258746932","8521479630","8529631470","5252354789","1021458745","8521479630","9632147852","7894215464","6663425175","2000147835","1258749630"];
   List contacts_email=["adalschmidt@gmail.com","alansmith@gmail.com","ameliajones@gmail.com","beyandavis@gmail.com","blakeweaver@gmail.com","christinajohnson@gmail.com","davidwilson@gmail.com","elizabethtaylor@gmail.com","hardikpandya@gmail.com","jaspritbumrah@gmail.com","klrahul@gmail.com","msdhoni@gmail.com","odinsonthor@gmail.com","ravindrajadeja@gmail.com","rishabhpant@gmail.com","rohitsharma@gmail.com","therock@gmail.com","tonystark@gmail.com","viratkohli@gmail.com","yuzvendrachahal@gmail.com"];
   List contacts_bithofday=["11 Aug","21 Jun","16 Oct","30 Nov","25 Sep","5 Aug","31 May","1 Oct","3 July","9 Sep","22 Aug","19 Nov","20 June","8 Sep","25 March","18 Feb","10 Dec","17 Feb","29 June","12 Dec",];
 
@@ -40,8 +40,10 @@ class _con_ScreenState extends State<con_Screen> {
                       Navigator.pushNamed(context,'setting', arguments: l1);},
                         child: Text("Settings",style: TextStyle(color: Colors.white),)),),
                     PopupMenuItem(child: InkWell(onTap: (){
-                      Navigator.pushNamed(context,'help', arguments: l1);},
-                         child: Text("Help & feedback",style: TextStyle(color: Colors.white),)),
+                      String link = "https://support.google.com/contacts/answer/9423168?hl=en";
+                      Uri url = Uri.parse(link);
+                      launchUrl(url);}
+                         ,child: Text("Help & feedback",style: TextStyle(color: Colors.white),)),
 
                     ),
                   ];
